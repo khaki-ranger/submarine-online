a38e4a176b3b4e1808f6af6660d7054cf19ef4db
Yohei Terashima <yamanogomora@gmail.com>
2019-05-11 12:20:20 +0000

    Add: プレイヤーとアイテムの情報をサーバーで生成してクライアントに送信する処理を実装

--- game_server/webSocketServer.js
+++ game_server/webSocketServer.js
@@ -6,14 +6,16 @@ function createWebSocketServer(io, game) {
     const displayName = socket.handshake.query.displayName;
     const thumbUrl = socket.handshake.query.thumbUrl;
 
-    console.log('WebSocket のコネクションがありました。');
-    socket.emit('start data', {});
-
-    socket.on('disconnect', () => {});
+    const startObj = game.newConnection(socket.id, displayName, thumbUrl);
+    socket.emit('start data', startObj);
+    
+    socket.on('disconnect', () => {
+      game.disconnect(socket.id);
+    });
   });
 
   const socketTicker = setInterval(() => {
-      rootIo.volatile.emit('map data', {}); // 全員に送信
+      rootIo.volatile.emit('map data', game.getMapData()); // 全員に送信
     },
     66);
 
